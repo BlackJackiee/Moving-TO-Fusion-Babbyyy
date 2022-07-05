55d7ad0d218d8ddede6fd2b1e194912524500969
Callme-jj <Wahab6518@gmail.com>
2022-07-05 13:53:49 +0300

    Finished States! Movin on to 'Changing Properties'!

--- src/client/FusionStates.story.lua
+++ src/client/FusionStates.story.lua
@@ -1,4 +1,5 @@
 --!strict
+print("----------------Started")
 --Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
@@ -6,15 +7,25 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 --Fusion
 local Fusion = require(ReplicatedStorage.Fusion)
+local State = Fusion.State
+local Computed = Fusion.Computed
 
 
 
+--Fusion States
+local numPlayers = State(5)
+local ComputedMsg = Computed(function()
+    return ("System: (%q) Are Currently In-Game!"):format(tostring(numPlayers:get()))
+end)
+
+print(ComputedMsg:get())
+numPlayers:set(12)
+print(ComputedMsg:get())
 
 
 --Creating the hoarcekat story
 return function(target)
     
     
-    print("Started")
-    return function() gui:Destroy() end
+    return function() end
 end
